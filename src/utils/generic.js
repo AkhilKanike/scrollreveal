02970d9743255fff697abc18cfa88ebf9afa8c1a
J.L. Makes <julianlloyd@gmail.com>
2016-11-27 22:55:15 +0100

    add new utility to check if object literal

--- src/utils/generic.js
+++ src/utils/generic.js
@@ -1,3 +1,7 @@
+export function isObject (object) {
+	return object !== null && typeof object === 'object' && object.constructor === Object;
+}
+
 export function logger (message) {
 	if (console) {
 		console.log(`ScrollReveal: ${message}`); // eslint-disable-line no-console
